c375cdf056d5976798b635c458cdb176b4b2521e
Xu Yihao <48053143+Yihao-Xu@users.noreply.github.com>
2023-06-16 19:36:37 +0800

    feat(PostgreSQL): select pushdown for PostgreSQL
    Query and dummy query with predicate pushdown are implemented

--- dataSources/postgresql/src/main/java/cn/edu/tsinghua/iginx/postgresql/tools/Constants.java
+++ dataSources/postgresql/src/main/java/cn/edu/tsinghua/iginx/postgresql/tools/Constants.java
@@ -32,11 +32,17 @@ public class Constants {
   public static final String QUERY_STATEMENT =
       "SELECT " + KEY_NAME + ", %s FROM %s WHERE %s ORDER BY " + KEY_NAME + ";";
 
+  public static final String QUERY_STATEMENT_WITH_JOIN =
+          "SELECT %s FROM %s WHERE %s ORDER BY %s;";
+
   public static final String QUERY_KEY_STATEMENT_WITHOUT_WHERE_CLAUSE =
       "SELECT %s FROM %s ORDER BY " + KEY_NAME + ";";
 
   public static final String CONCAT_QUERY_STATEMENT_WITHOUT_WHERE_CLAUSE =
       "SELECT concat(%s) AS " + KEY_NAME + ", %s FROM %s ORDER BY " + KEY_NAME + ";";
+
+  public static final String CONCAT_QUERY_STATEMENT_WITH_WHERE_CLAUSE =
+          "SELECT concat(%s) AS " + KEY_NAME + ", %s FROM %s WHERE %s ORDER BY " + KEY_NAME + ";";
 
   public static final String CREATE_TABLE_STATEMENT =
       "CREATE TABLE %s (" + KEY_NAME + " BIGINT NOT NULL, %s %s, PRIMARY KEY(" + KEY_NAME + "));";
